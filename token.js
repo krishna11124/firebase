ee6949923d3a8d86ee0b3d3eb206a17b6b205495
krishna11124 <38029697+krishna11124@users.noreply.github.com>
2025-04-30 14:58:50 +0530

    updated

---
+++ token.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const { GoogleAuth } = require('google-auth-library');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+
+app.use(cors()); 
+
+async function getAccessToken() {
+  const credentials = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_JSON);
+
+  const auth = new GoogleAuth({
+    credentials: credentials,
+    scopes: ['https://www.googleapis.com/auth/firebase.messaging'],
+  });
+  const client = await auth.getClient();
+  const accessToken = await client.getAccessToken();
+  return accessToken.token;
+}
+
+app.get('/get-token', async (req, res) => {
+  try {
+    const token = await getAccessToken();
+    res.json({ accessToken: token });
+  } catch (error) {
+    console.error('Error getting token:', error);
+    res.status(500).json({ error: 'Failed to generate access token' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
